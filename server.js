de29b9b2329ff8cf268d4a26e697c0846a93f1b7
kamilugn-zaj <kamilugniewski@gmail.com>
2026-03-25 12:21:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
+app.set('trust proxy', 1);
+
+const apiLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    message: { error: 'Nie spamuj! Poczekaj minutę.' }
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/', apiLimiter);
 
 app.get('/api/cats', async (req, res) => {
     try {
